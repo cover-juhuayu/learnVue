8b15f66c7ea0c99de6b8c5c98c8a9c048e67addd
juhuayu <cover@juhuayu.com>
2022-03-22 17:38:59 +0800

    完成增加vuex的使用-vue2-vuex3.6

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <h1>测试</h1>{{msg}}
+    <br>
+    <h1>这是vuex上state的值：{{this.$store.state.count}}</h1>
+    <button @click="add">+</button>
+    <div>{{count1}}</div>
+    <button @click="del">-</button>
     <header>
     <!-- router-link 定义点击后导航到哪个路径下 -->
       <router-link to="/home">Home</router-link>
@@ -14,12 +19,35 @@
 
 <script>
 import router from './router';
+import{ mapState, mapGetters } from 'vuex'
 export default {
   name: 'App',
   router,
   data(){
     return{
       msg:"App"
+    }
+  },
+  computed: {
+    vuexMsg(){
+      return this.$store.state.msg
+    },
+    ...mapState({
+      count1:state=>state.count
+    }),
+    ...mapGetters([
+      'getCount',
+      'isShow'
+    ])
+  },
+  methods: {
+    add(){
+      // this.$store.commit('addCount', 1)
+      this.$store.dispatch('getAddCount', 1)
+    },
+    del(){
+      // this.$store.commit('delCount', 1)
+      this.$store.dispatch('getDelCount', 1)
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router.js"    // import router 的router 一定要小写， 不要写成Router, 否则报 can't match的报错
+import store from "./store/index.js"
 
 Vue.config.productionTip = false
-import router from "./router.js"    // import router 的router 一定要小写， 不要写成Router, 否则报 can't match的报错
+
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
 
